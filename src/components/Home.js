38a7ef0613cea7eca1597af97bec96b1eebc9239
abda-torey <abdiazizibrahim.mohamed@student.griffith.ie>
2024-03-02 21:29:00 +0000

    added download button

--- src/components/Home.js
+++ src/components/Home.js
@@ -30,14 +30,25 @@ const Home = () => {
       <div className="cavani_tm_home relative w-full h-full flex items-center">
         <div className="content pl-[100px]">
           <h3 className="name text-[68px] font-bold uppercase mb-[30px]">
-            Abdiaziz  Mohamed
+            Abdiaziz Mohamed
           </h3>
           <span className="line inline-block w-[70px] h-[5px] bg-[#333] mb-[30px]" />
           <TypingAnimation />
-          <div className="cavani_tm_button transition_link">
-            <a href="#contact" onClick={() => navChange("contact")}>
-              Get in Touch
-            </a>
+          <div className="flex">
+            <div className="cavani_tm_button transition_link">
+              <a href="#contact" onClick={() => navChange("contact")}>
+                Get in Touch
+              </a>
+            </div>
+            <div className="cavani_tm_button_download transition_link">
+              <a
+                href="https://drive.google.com/uc?export=download&id=1fSqg4lIE35dI-3_p860GtPd5faHhS57u"
+                download
+               
+              >
+                Download CV
+              </a>
+            </div>
           </div>
         </div>
       </div>
